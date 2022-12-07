1e5943cefc44cd23dd53bb409383d379e7e7519c
developer-kawsar <kawsar.qtec@gmail.com>
2022-12-07 18:33:22 +0600

    login added

---
+++ assets/js/config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          "dark-purple": "#081A51",
+          "primary": "#D7E3DE",
+          "primarylight": "#E7F2EE",
+          "light-white": "rgba(255,255,255,0.17)",
+        }
+      }
+    }
+  }
